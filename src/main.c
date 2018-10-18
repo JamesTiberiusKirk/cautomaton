4d395bb7194abf1f229c6fa8dc0472b56fbf940d
darthvader <dumitru.v.dv@gmail.com>
2018-10-19 00:40:55 +0100

    Started working on converting int to binary

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int charDecToBin(char ruleDec){
+    char* p;
+    int ruleInt = strtol(ruleInt,&p,10);
+    
+    
+    
+{
+
+int main(int argc char* argv[]){
+    char ruleDec = argv[1];
+    charDecToBin(ruleDec);
+    
+    return 0;
+}
